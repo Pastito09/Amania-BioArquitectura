df7c525615231c9bec9c9cf472a020e674e0935b
Manuel Salvadores <patriciozhogan@gmail.com>
2024-10-10 11:33:43 -0300

    colores y tamaño de mb

--- src/components/paginas/obras/Obras.tsx
+++ src/components/paginas/obras/Obras.tsx
@@ -3,7 +3,7 @@ import ObraCard from './ObraCard';
 export const Obras = () => {
   return (
     <>
-      <div className='grid grid-cols1 md:grid-cols-2 mt-10 gap-2 md:gap-10 mb-2'>
+      <div className='grid grid-cols1 md:grid-cols-2 mt-10 gap-2 md:gap-10 mb-10'>
         <ObraCard />
 
         <ObraCard />
